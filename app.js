ac608766adc202896613b2f797e47f2853f73e75
Coda Bool <CodaBool@pm.me>
2021-01-08 11:15:27 -0500

    start endpoints and pools

--- app.js
+++ app.js
@@ -8,15 +8,29 @@ const port = process.env.PORT || 3000 // added for heroku deploy to work
 // middleware
 app.use(cors())
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const pool_home = new Pool({
+  connectionString: process.env.HOME_URI,
+  ssl: { rejectUnauthorized: false },
+  max: 19, // 10 default
+  connectionTimeoutMillis: 10000, // 0 default
+  idleTimeoutMillis: 10000, // 10000 default
+})
+const pool_market = new Pool({
+  connectionString: process.env.MARKET_URI,
+  ssl: { rejectUnauthorized: false },
+  max: 19, // 10 default
+  connectionTimeoutMillis: 10000, // 0 default
+  idleTimeoutMillis: 10000, // 10000 default
+})
+const pool_social = new Pool({
+  connectionString: process.env.SOCIAL_URI,
   ssl: { rejectUnauthorized: false },
   max: 19, // 10 default
   connectionTimeoutMillis: 10000, // 0 default
   idleTimeoutMillis: 10000, // 10000 default
 })
 
-async function query(q, values) {
+async function query(q, values, pool) {
   return await pool.query(q, values)
     .then(res => {
       return res
@@ -27,20 +41,64 @@ async function query(q, values) {
     })
 }
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
   res.status(200).send('try an endpoint')
 })
 
-app.get("/loaderio-ba7db8e3e245c156d37018ba8f199290", (req, res) => {
-  res.status(200).send('loaderio-ba7db8e3e245c156d37018ba8f199290')
-})
-app.get("/caffine", (req, res) => {
-  res.status(200).send('*sips*\nthanks')
+app.get("/verify", async(req, res) => {
+  console.log('is this a key?', req.query)
+  try {
+    if (req.query === process.env.POOL_KEY) {
+      res.status(200).json(result)
+    } else {
+      res.status(403).send('unauthorized')
+    }
+  } catch(err) {
+    res.status(400).send(JSON.stringify(err))
+  }
 })
 
-app.get("/test", async(req, res) => {
-  const result = await query('SELECT * FROM post', [])
-  res.status(200).json(result)
+app.get("/home", async(req, res) => {
+  // use api key
+  if (true) {
+    const result = await query('SELECT * FROM post', [], pool_home)
+    res.status(200).json(result)
+  } else {
+    res.status(403).send('unauthorized')
+  }
+})
+app.get("/market", async(req, res) => {
+  // use api key
+  if (true) {
+    const result = await query('SELECT * FROM post', [], pool_market)
+    res.status(200).json(result)
+  } else {
+    res.status(403).send('unauthorized')
+  }
+  if (true) {
+    const result = await query('SELECT * FROM post', [], pool_social)
+    res.status(200).json(result)
+  } else {
+    res.status(403).send('unauthorized')
+  }
+})
+app.get("/social", async(req, res) => {
+  // use api key
+  if (true) {
+    const result = await query('SELECT * FROM post', [], pool_social)
+    res.status(200).json(result)
+  } else {
+    res.status(403).send('unauthorized')
+  }
 })
 
 app.listen(port, () => console.log(`\n----> http://localhost:${port}\n`));
+
+/*
+only get 10000 rows
+- can have multiple pg which are pooled
+recieve
+query, values, apikey, which db
+return db result
+
+*/
